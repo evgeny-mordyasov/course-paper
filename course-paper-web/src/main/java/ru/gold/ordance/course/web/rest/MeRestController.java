42687775cd5ddafaa67ccf60e4c4ecd2e864875f
evgeny_mordyasov <michisig11i7@gmail.com>
2022-12-30 18:36:14 +0700

    create course-paper-persistence module

--- course-paper-web/src/main/java/ru/gold/ordance/course/web/rest/MeRestController.java
+++ course-paper-web/src/main/java/ru/gold/ordance/course/web/rest/MeRestController.java
@@ -2,8 +2,8 @@ package ru.gold.ordance.course.web.rest;
 
 import org.springframework.web.bind.annotation.*;
 import ru.gold.ordance.course.internal.api.domain.Response;
-import ru.gold.ordance.course.internal.api.domain.authorization.request.AuthorizationSignInRequest;
-import ru.gold.ordance.course.web.service.AuthorizationWebService;
+import ru.gold.ordance.course.internal.api.domain.client.request.ClientGetByIdRequest;
+import ru.gold.ordance.course.web.service.ClientWebService;
 
 import static ru.gold.ordance.course.web.utils.RequestUtils.JSON;
 import static ru.gold.ordance.course.web.utils.RequestUtils.execute;
@@ -11,14 +11,14 @@ import static ru.gold.ordance.course.web.utils.RequestUtils.execute;
 @RestController
 @RequestMapping("/api/v1/me")
 public class MeRestController {
-    private final AuthorizationWebService service;
+    private final ClientWebService service;
 
-    public MeRestController(AuthorizationWebService service) {
+    public MeRestController(ClientWebService service) {
         this.service = service;
     }
 
-    @PostMapping(consumes = JSON, produces = JSON)
-    public Response signIn(@RequestBody AuthorizationSignInRequest rq) {
-        return execute(() -> service.signIn(rq));
+    @GetMapping(value = "/{clientId}", produces = JSON)
+    public Response signIn(@PathVariable Long clientId) {
+        return execute(() -> service.findById(new ClientGetByIdRequest(clientId)));
     }
 }
